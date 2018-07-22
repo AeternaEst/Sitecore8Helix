4f6b745e39dbf431928f78fc5adab85b3931a4c6
Nicholas Lindboe <nicholaslindboe@gmail.com>
2018-07-22 22:52:38 +0200

    First git commit

---
+++ Src/Project/Sitecore8Helix/code/Controllers/TestController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Sitecore8Helix.Website.Controllers
+{
+    public class TestController : Controller
+    {
+        public ActionResult Test()
+        {
+            return View();
+        }
+    }
+}
