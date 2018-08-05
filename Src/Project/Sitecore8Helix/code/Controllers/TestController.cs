4c2ee0b847f0092b1a7c6c60f340ec66ca1ed028
Nicholas Lindboe <nicholaslindboe@gmail.com>
2018-08-05 21:47:41 +0200

    Basic Solr Search Ready

--- Src/Project/Sitecore8Helix/code/Controllers/TestController.cs
+++ Src/Project/Sitecore8Helix/code/Controllers/TestController.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Sitecore.ContentSearch;
+using Sitecore.ContentSearch.Linq;
+using Sitecore.ContentSearch.Utilities;
+using Sitecore8Helix.Website.Models;
 
 namespace Sitecore8Helix.Website.Controllers
 {
@@ -11,6 +15,41 @@ namespace Sitecore8Helix.Website.Controllers
         public ActionResult Test()
         {
             return View();
+        }
+
+        public ActionResult Stores()
+        {
+            var title = Sitecore.Context.Item["Headline"];
+            var text = Sitecore.Context.Item["Text"];
+
+            ViewBag.Title = title;
+            ViewBag.Text = text;
+
+            var index = ContentSearchManager.GetIndex("sitecore_master_index");
+            List<Store> stores = new List<Store>();
+            using (var searchContext = index.CreateSearchContext())
+            {
+                var query = searchContext.GetQueryable<StoreSearchResultItem>().
+                    Where(item => item.TemplateName == "Store");
+
+                var results = query.GetResults();
+                foreach (var result in results.Hits)
+                {
+                    stores.Add(new Store
+                    {
+                        Name = result.Document.StoreName.FirstOrDefault(),
+                        Description = result.Document.Description.FirstOrDefault(),
+                        City = result.Document.City,
+                        Country = result.Document.Country,
+                        Street = result.Document.Street,
+                        StreetNumber = result.Document.StreetNumber,
+                        Type = result.Document.Type,
+                        ZipCode = result.Document.ZipCode
+                    });
+                }
+            }
+
+            return View(stores);
         }
     }
 }
---
+++ Src/Project/Sitecore8Helix/code/Models/Store.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Sitecore8Helix.Website.Models
+{
+    public class Store
+    {
+        public string Name
+        {
+            get;
+            set;
+        }
+
+        public string Description
+        {
+            get;
+            set;
+        }
+
+        public string Country
+        {
+            get;
+            set;
+        }
+
+        public string City
+        {
+            get;
+            set;
+        }
+
+        public string Street
+        {
+            get;
+            set;
+        }
+
+        public string StreetNumber
+        {
+            get;
+            set;
+        }
+
+        public string ZipCode
+        {
+            get;
+            set;
+        }
+
+        public string Type
+        {
+            get;
+            set;
+        }
+    }
+}
