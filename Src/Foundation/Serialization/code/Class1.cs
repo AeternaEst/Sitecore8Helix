3a8429a6cf34aff5fe67a1e50dd58b6a3c205852
DIS\nli <nli@dis-play.dk>
2018-11-02 13:42:19 +0100

    Reference Configy in code so it gets copied to Webroot

---
+++ Src/Foundation/Serialization/code/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Configy.Containers;
+
+namespace Sitecore8Helix.Foundation.Serialization
+{
+    public class Class1
+    {
+        protected IContainer Container;
+    }
+}
