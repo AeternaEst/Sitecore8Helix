715a66342250062035d59d763bf3228d5122a942
Nicholas Lindboe <nicholaslindboe@gmail.com>
2018-11-07 22:08:15 +0100

    Initial setup for Media Frame and Grid Layout

---
+++ Src/Feature/Media/code/Controllers/MediaContentController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Sitecore8Helix.Feature.Media.Controllers
+{
+    public class MediaContentController : Controller
+    {
+
+        public ActionResult MediaFrame()
+        {
+            return View("~/Views/Media/MediaFrame.cshtml");
+        }
+    }
+}
